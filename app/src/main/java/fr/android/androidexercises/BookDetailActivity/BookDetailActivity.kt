73428bac7e5e4f61a0f7bd8d21b07b9e30588d67
unknown <alexis.delforges@gmail.com>
2019-11-12 15:49:21 +0100

    Add shopping cart with correct number of books in sharedPrefs

--- app/src/main/java/fr/android/androidexercises/BookDetailActivity/BookDetailActivity.kt
+++ app/src/main/java/fr/android/androidexercises/BookDetailActivity/BookDetailActivity.kt
@@ -2,17 +2,21 @@ package fr.android.androidexercises.BookDetailActivity
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.MenuItemCompat
 import com.squareup.picasso.Picasso
 import fr.android.androidexercises.Global.Book
 import fr.android.androidexercises.MainBookActivity.View.BookItemView
 import fr.android.androidexercises.R
 
-class BookDetailActivity : AppCompatActivity(), BookItemView.BookBuyer {
+class BookDetailActivity : AppCompatActivity(){
 
     private var title: TextView? = null
     private var price: TextView? = null
@@ -20,11 +24,18 @@ class BookDetailActivity : AppCompatActivity(), BookItemView.BookBuyer {
     private var cover: ImageView? = null
     private var book : Book? = null
 
+    private var shoppingNumbers : Int = 0
+    var textCartItemCount: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_detail)
 
+        val toolbar = findViewById<Toolbar>(R.id.toolbar2)
+        setSupportActionBar(toolbar)
+
         book = intent.getParcelableExtra(getString(R.string.detail_intent_key))
+        shoppingNumbers = intent.getIntExtra(getString(R.string.detail_intent_items), 0)
 
 
         //Lot of boilerplate than can be erased with databinding.
@@ -38,15 +49,33 @@ class BookDetailActivity : AppCompatActivity(), BookItemView.BookBuyer {
         description?.text = book?.synopsis?.get(0)
         Picasso.get().load(book?.cover).into(cover)
 
-        val buyButton : Button = findViewById(R.id.buy_button_details)
-        buyButton.setOnClickListener {
-            Log.i("bookBuy", "Book is going to be buy "+book?.title)
-            this.userWantToBuyBook(book = book!!)
-        }
     }
 
-    override fun userWantToBuyBook(book: Book) {
-        Toast.makeText(this, "Shop Not Available for " + book.title, Toast.LENGTH_LONG).show()
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_library, menu)
+
+        //For shopping cart
+        val menuItem = menu.findItem(R.id.action_shop)
+        val actionView = MenuItemCompat.getActionView(menuItem)
+        actionView.setOnClickListener { onOptionsItemSelected(menuItem) }
+        textCartItemCount = actionView.findViewById(R.id.cart_badge) as TextView
+        textCartItemCount!!.text = "" + shoppingNumbers
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            R.id.action_home -> {
+                Toast.makeText(this, "Go at home", Toast.LENGTH_SHORT).show()
+                onBackPressed()
+            }
+            R.id.action_shop -> {
+                Toast.makeText(this, "Shop Not Available", Toast.LENGTH_LONG).show()
+            }
+        }
+        return true
     }
 
 }
--- app/src/main/java/fr/android/androidexercises/MainBookActivity/LibraryActivity.kt
+++ app/src/main/java/fr/android/androidexercises/MainBookActivity/LibraryActivity.kt
@@ -1,7 +1,10 @@
 package fr.android.androidexercises.MainBookActivity
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -13,31 +16,63 @@ import fr.android.androidexercises.R
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
-
-
-
+import android.widget.TextView
+import androidx.core.view.MenuItemCompat
 
 //Main activity. Work with a presenter to get data from and a ListView to display them (adapter). Responsible of navigation
 class LibraryActivity : AppCompatActivity(), LibraryPresenter.BookReceiver, BookItemView.BookBuyer, BookItemView.BookDescriptionHandler {
 
+    var textCartItemCount: TextView? = null
+    private var shoppingNumbers : Int = 0
+
+    private var PRIVATE_MODE = 0
+    private val PREF_NAME = "library_cart"
+    lateinit var sharedPref: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_library)
+
         val toolbar = findViewById<Toolbar>(R.id.toolbar2)
         setSupportActionBar(toolbar)
+
         LibraryPresenter().init(this, this)
+
+        if(savedInstanceState != null){
+            shoppingNumbers = savedInstanceState.getInt(getString(R.string.bought_items))
+        }
+
+        Log.i("shopping number", shoppingNumbers.toString())
+
+        sharedPref = getSharedPreferences(PREF_NAME, PRIVATE_MODE)
+        Log.i("sharedPrefs", sharedPref.getInt("c8fabf68-8374-48fe-a7ea-a00ccd07afff", 666).toString())
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.menu_library, menu)
+
+        //For shopping cart
+        val menuItem = menu.findItem(R.id.action_shop)
+        val actionView = MenuItemCompat.getActionView(menuItem)
+        actionView.setOnClickListener { onOptionsItemSelected(menuItem) }
+        textCartItemCount = actionView.findViewById(R.id.cart_badge) as TextView
+        textCartItemCount!!.text = "" + shoppingNumbers
+        
         return true
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(getString(R.string.bought_items), shoppingNumbers!!)
+        Log.i("shopping number", shoppingNumbers.toString())
+        super.onSaveInstanceState(outState)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.getItemId()) {
             R.id.action_home -> {
                 Toast.makeText(this, "Already at home", Toast.LENGTH_SHORT).show()
+
             }
             R.id.action_shop -> {
                 Toast.makeText(this, "Shop Not Available", Toast.LENGTH_LONG).show()
@@ -49,16 +84,36 @@ class LibraryActivity : AppCompatActivity(), LibraryPresenter.BookReceiver, Book
     override fun OnBookReceived(books: List<Book>){
         val listView = findViewById<ListView>(R.id.bookListView)
         listView.adapter = BookAdapter(this, books)
+
+        //get the number of books bought by user in userPrefs
+        var buyCounter =0
+        for (book in books){
+            buyCounter += sharedPref.getInt(book.isbn, 0)
+        }
+        shoppingNumbers = buyCounter
+        textCartItemCount!!.text = "" + shoppingNumbers
+
     }
 
     override fun userWantToBuyBook(book: Book) {
         Toast.makeText(this, "Shop Not Available for " + book.title, Toast.LENGTH_LONG).show()
+
+        shoppingNumbers = shoppingNumbers?.plus(1)
+        //could be handle by databinding
+        textCartItemCount!!.text = "" + shoppingNumbers
+
+        //put a new book in the sharedPrefs file
+        val bookItems = sharedPref.getInt(book.isbn, 0)
+        val editor = sharedPref.edit()
+        editor.putInt(book.isbn, bookItems+1)
+        editor.apply()
     }
 
     override fun userWantToGetInfoOnBook(book: Book) {
         Toast.makeText(this, "Get description for " + book.title, Toast.LENGTH_LONG).show()
         val intent = Intent(this, BookDetailActivity::class.java)
         intent.putExtra(getString(R.string.detail_intent_key), book)
+        intent.putExtra(getString(R.string.detail_intent_items), shoppingNumbers)
         startActivity(intent)
     }
 }
